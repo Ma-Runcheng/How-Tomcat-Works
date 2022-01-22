fe6d4cffab47ffaaab5e521fa1b804bdcc0d3bff
MaRuncheng <MaRuncheng0@gmail.com>
2022-01-22 16:36:46 +0800

    tomcat

---
+++ how-tomcat-works-ex03/src/main/java/ex03/pyrmont/connector/http/Constants.java
@@ -0,0 +1,12 @@
+package ex03.pyrmont.connector.http;
+import java.io.File;
+
+public final class Constants {
+  public static final String Module = "how-tomcat-works-ex03";
+  public static final String WEB_ROOT =
+    System.getProperty("user.dir") +File.separator + Module + File.separator  + "webroot";
+  public static final String Package = "ex03.pyrmont.connector.http";
+  public static final int DEFAULT_CONNECTION_TIMEOUT = 60000;
+  public static final int PROCESSOR_IDLE = 0;
+  public static final int PROCESSOR_ACTIVE = 1;
+}
